8b6a0cca140b24d692e087b82e81906449bd73b1
FraCuboni <fracuboni@gmail.com>
2024-09-04 15:09:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cats&Dogs</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <!-- inizio foreach -->
+            <?php
+            ?>
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    <h5><?php  ?></h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><?php  ?></li>
+                    <li class="list-group-item"><?php  ?></li>
+                </ul>
+            </div>
+            <?php ?>
+            <!-- fine foreach -->
+        </div>
+    </div>
+</body>
+
+</html>
